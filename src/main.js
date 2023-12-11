fb5976d80bfd6bf60ec2ea7fa8fa07e3541b8a8e
RUBENGAENS <12101888@student.pxl.be>
2023-12-11 09:30:21 +0100

    push

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './scss/main.scss'
+
+import { createApp } from 'vue'
+import router from './router'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.use(router);
+app.mount('#app')
